ba5a3ae8c2a5c6a3993e73cfeabca51371edeabe
unknown <you@example.com>
2025-03-08 18:21:53 -0500

    fixed sheet name

---
+++ testfile.js
@@ -0,0 +1,6 @@
+// File: src/data/employeeService.gs (or in a separate test file)
+function testGenerateJccId() {
+  // This calls the generateJccId() function in employeeService.gs
+  var newId = generateJccId();
+  Logger.log("Generated JCC ID: " + newId);
+}
